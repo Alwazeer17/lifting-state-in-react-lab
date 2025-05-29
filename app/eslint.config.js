2528ea8fc24b07006ca97a7e83d69f99f71fc200
Alwazeer <142330212+Alwazeer17@users.noreply.github.com>
2025-05-29 23:59:41 +0300

    zero

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,7 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';           // <-- add this import
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -17,22 +18,22 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
-  ...js.configs.recommended.rules,
-  ...react.configs.recommended.rules,
-  ...react.configs['jsx-runtime'].rules,
-  ...reactHooks.configs.recommended.rules,
-  'react/jsx-no-target-blank': 'off',
-  'react-refresh/only-export-components': [
-    'warn',
-    { allowConstantExport: true },
-  ],
-  'react/prop-types': 'off',            // add this line
-  'react/no-unescaped-entities': 'off', // add this line
-},
-
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off',            // disable prop-types warnings
+      'react/no-unescaped-entities': 'off', // allow contractions in JSX
+    },
   },
-]
+];
